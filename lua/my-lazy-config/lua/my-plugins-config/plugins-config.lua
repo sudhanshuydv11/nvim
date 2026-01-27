932ebfe441bac69ee1685dd5e74e8e901f67db1f
sudhanshu yadav <sudhanshu.sy2000@gmail.com>
2026-01-27 20:16:51 +0530

    improving tab UI

--- lua/my-lazy-config/lua/my-plugins-config/plugins-config.lua
+++ lua/my-lazy-config/lua/my-plugins-config/plugins-config.lua
@@ -33,54 +33,6 @@ require("CopilotChat").setup({
 	auto_insert_mode = true,
 	insert_at_end = true,
 })
-
-local theme = {
-	fill = "TabLineFill",
-	-- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
-	head = "TabLine",
-	current_tab = "TabLineSel",
-	tab = "TabLine",
-	win = "TabLine",
-	tail = "TabLine",
-}
-require("tabby.tabline").set(function(line)
-	return {
-		{
-			{ "  ", hl = theme.head },
-			line.sep("", theme.head, theme.fill),
-		},
-		line.tabs().foreach(function(tab)
-			local hl = tab.is_current() and theme.current_tab or theme.tab
-			return {
-				line.sep("", hl, theme.fill),
-				tab.is_current() and "" or "󰆣",
-				tab.number(),
-				tab.name(),
-				tab.close_btn(""),
-				line.sep("", hl, theme.fill),
-				hl = hl,
-				margin = " ",
-			}
-		end),
-		line.spacer(),
-		line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-			return {
-				line.sep("", theme.win, theme.fill),
-				win.is_current() and "" or "",
-				win.buf_name(),
-				line.sep("", theme.win, theme.fill),
-				hl = theme.win,
-				margin = " ",
-			}
-		end),
-		{
-			line.sep("", theme.tail, theme.fill),
-			{ "  ", hl = theme.tail },
-		},
-		hl = theme.fill,
-	}
-end)
-
 require("vgit").setup()
 require("gitsigns").setup()
 require("gitblame").setup()
@@ -102,3 +54,42 @@ require("nvim-treesitter.configs").setup({
 })
 
 require("fzf-lua").setup({})
+
+local buf_name = require('tabby.feature.buf_name')
+local devicons = require('nvim-web-devicons')
+
+require('tabby.tabline').use_preset('active_wins_at_tail', {
+  nerdfont = true,
+  theme = {
+    fill = { fg = '#ebdbb2', bg = '#282828' },
+    head = { fg = '#b8bb26', bg = '#282828', style = 'bold' },
+    current_tab = { fg = '#282828', bg = '#98971a', style = 'bold' }, -- greenish
+    tab = { fg = '#ebdbb2', bg = '#3c3836' },
+    win = { fg = '#b8bb26', bg = '#3c3836' },
+    tail = { fg = '#b8bb26', bg = '#282828', style = 'bold' },
+  },
+  tab_name = {
+    name_fallback = function(tabid)
+      local wins = vim.api.nvim_tabpage_list_wins(tabid)
+      for i = #wins, 1, -1 do
+        local win = wins[i]
+        if vim.api.nvim_win_is_valid(win) then
+          local buf = vim.api.nvim_win_get_buf(win)
+          if vim.api.nvim_buf_is_valid(buf)
+            and vim.bo[buf].buftype == ''
+            and vim.bo[buf].modifiable
+          then
+            local icon = devicons.get_icon(vim.api.nvim_buf_get_name(buf)) or '' -- remove default glyph
+            local name = buf_name.get(win, { mode = 'tail' })
+            local mod = vim.bo[buf].modified and ' ●' or ''
+            return ' ' .. icon .. ' ' .. name .. mod .. ' '
+          end
+        end
+      end
+      return ' [No File] '
+    end,
+    padding = { left = 1, right = 1 },
+  },
+})
+
+
